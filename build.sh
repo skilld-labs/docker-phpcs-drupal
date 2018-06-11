eb51fe855e93411245b3b87ee6f21a561a501892
Andy Postnikov <apostnikov@gmail.com>
2018-06-11 17:31:51 +0300

    Fix SF sniffers build

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ docker build --pull \
   --build-arg VCS_REF=`git rev-parse --short HEAD` \
   -t skilldlabs/docker-phpcs-drupal .
 
-cd ci
+docker build --pull \
+  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+  --build-arg VCS_REF=`git rev-parse --short HEAD` \
+  -t skilldlabs/docker-phpcs-drupal:symfony symfony
 
-docker build -t skilldlabs/docker-phpcs-drupal:ci .
+cd ci
+#docker build -t skilldlabs/docker-phpcs-drupal:ci .
